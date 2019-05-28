708552957d66b672f10de9cbe5171ac9860d9b8a
Jarrod Davis <developer@jarrodldavis.com>
2019-05-28 09:16:47 -0600

    Namespace element options

--- src/options.ts
+++ src/options.ts
@@ -10,8 +10,8 @@ export interface PluginOptions {
     svelteStrictMode: boolean;
     svelteBracketNewLine: boolean;
     svelteAllowShorthand: boolean;
-    inlineElements: TagName[],
-    preformattedElements: TagName[];
+    svelteInlineElements: TagName[],
+    sveltePreformattedElements: TagName[];
 }
 
 export const options: Record<keyof PluginOptions, SupportOption> = {
@@ -38,14 +38,12 @@ export const options: Record<keyof PluginOptions, SupportOption> = {
         default: false,
         description: 'Put the `>` of a multiline element on a new line',
     },
-<<<<<<< HEAD
     svelteAllowShorthand: {
         type: 'boolean',
         default: true,
         description: 'Option to enable/disable component attribute shorthand if attribute name and expressions are same',
     },
-=======
-    inlineElements: {
+    svelteInlineElements: {
         type: 'choice',
         // @ts-ignore -- typings for `SupportOption` doesn't support arrays even though that's a valid type
         // https://github.com/prettier/prettier/blob/3654108ebe028fbc3063ceccb3c4ce0d4164510a/src/main/core-options.js#L167-L178
@@ -54,7 +52,7 @@ export const options: Record<keyof PluginOptions, SupportOption> = {
         array: true,
         choices: allElements.map(value => ({ value }))
     },
-    preformattedElements: {
+    sveltePreformattedElements: {
         type: 'choice',
         // @ts-ignore -- typings for `SupportOption` doesn't support arrays even though that's a valid type
         // https://github.com/prettier/prettier/blob/3654108ebe028fbc3063ceccb3c4ce0d4164510a/src/main/core-options.js#L167-L178
